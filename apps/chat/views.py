00487767f27fbbb1c7adbf14a8470e3844c75413
Afonso Machado <afonsosmachado@gmail.com>
2024-10-22 18:41:42 -0300

    feat: exibindo todas as mensagens e salas

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from .models import Message
+from .models import Message, Room
 
 # Create your views here.
 
 
 def home(request):
     messages = Message.objects.all()
+    rooms = Room.objects.all()
     return render(request, 'chat/home.html', {
-        'messages': messages
+        'messages': messages,
+        'rooms': rooms
     })
